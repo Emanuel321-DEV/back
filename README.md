389612477cdfc7794ac3ee4854f1cc6f8e14f201
Emanuel321-DEV <emanuelvieiradev@gmail.com>
2022-11-30 09:29:38 -0300

    add readme md

---
+++ README.md
@@ -0,0 +1,32 @@
+# Backend PontoGO
+
+## Requirements
+
+    - NPM or Yarn
+    - Git
+    - Postgres Db running
+    - NodeJS
+
+## Get Started
+
+1 - Clone the repo 
+2 - Into project directory execute 'npm i' or 'yarn' (install all dependencies)
+3 - Start your database
+4 - In the .env file add settings of your database
+    Example
+
+        TYPEORM_HOST=localhost
+        TYPEORM_PORT=5432
+        TYPEORM_TYPE=postgres
+        ...
+
+5- Into project directory execute 'yarn start:dev' or 'npm run start:dev'
+
+6 - Follow the steps described in LINKPROJETOFRONTEND
+
+## Main tecnologies
+
+    NestJS
+    Postgres
+    Typeorm
+    JWT
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { RegisteredTimeModule } from './registered_time/registered_time.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { User } from './user/entities/user.entity';
 import { RegisteredTime } from './registered_time/entities/registered_time.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: "postgres",
-      username: "postgres",
-      password: "docker",
-      database: "postgres",
-      host: "localhost",
-      port: 5432,
+      type: process.env.TYPEORM_TYPE,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATA,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
       entities: [User, RegisteredTime],
       synchronize: true
-    }),
+    } as TypeOrmModuleOptions),
     UserModule, 
     RegisteredTimeModule, 
     AuthModule
